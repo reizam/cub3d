eac4216880cdab4935300246e0710b8e8788ef65
ReiZam <kmazier@student.42.fr>
2020-12-16 17:39:49 +0100

    add screen

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 SRC_DIR		= ./srcs/
 
-SRC 		= ${SRC_DIR}ft_strdup.c ${SRC_DIR}start.c ${SRC_DIR}parse.c ${SRC_DIR}utils.c ${SRC_DIR}ft_split.c ${SRC_DIR}ft_atoi.c ${SRC_DIR}parse_rgb.c ${SRC_DIR}parse_map.c ${SRC_DIR}ft_strcmp.c ${SRC_DIR}close.c ${SRC_DIR}ft_putstr_fd.c ${SRC_DIR}get_next_line.c ${SRC_DIR}ft_strjoin.c
+SRC 		= ${SRC_DIR}screen.c ${SRC_DIR}ft_strdup.c ${SRC_DIR}start.c ${SRC_DIR}parse.c ${SRC_DIR}utils.c ${SRC_DIR}ft_split.c ${SRC_DIR}ft_atoi.c ${SRC_DIR}parse_rgb.c ${SRC_DIR}parse_map.c ${SRC_DIR}ft_strcmp.c ${SRC_DIR}close.c ${SRC_DIR}ft_putstr_fd.c ${SRC_DIR}get_next_line.c ${SRC_DIR}ft_strjoin.c
 OBJS 		= ${SRC:.c=.o}
 
 NAME 		= Cub3D
---
+++ srcs/screen.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   screen.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kmazier <kmazier@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/16 17:37:37 by kmazier           #+#    #+#             */
+/*   Updated: 2020/12/16 17:37:37 by kmazier          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cub3d.h"
+
+void    ft_open_screen(t_cub *cub)
+{
+    void    *mlx_ptr;
+    void    *win_ptr;
+
+    mlx_ptr = mlx_init();
+    win_ptr = mlx_new_window(mlx_ptr, cub->width, cub->height, "Cub3d");
+}
