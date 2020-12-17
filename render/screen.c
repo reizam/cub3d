0bb94170467a55d5c05ab2e38acd9e680af1c1aa
ReiZam <kmazier@student.42.fr>
2020-12-17 16:41:17 +0100

    free

--- render/screen.c
+++ render/screen.c
@@ -66,6 +66,8 @@ int     ft_render_screen(t_vars *vars)
     x = 0;
     hit = 0;
     img_ptr = mlx_new_image(vars->mlx_ptr, vars->cub->width, vars->cub->height);
+    if (vars->addr)
+        free(vars->addr);
     vars->addr = mlx_get_data_addr(img_ptr, &vars->bits_per_pixel, &vars->line_length,
                                  &vars->endian);
     while (x < vars->cub->width)
