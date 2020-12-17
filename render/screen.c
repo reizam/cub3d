40dd97bc0ce932388940c44ddb58bab03b6217f2
ReiZam <kmazier@student.42.fr>
2020-12-17 16:21:06 +0100

    fix put pixel

--- render/screen.c
+++ render/screen.c
@@ -66,7 +66,7 @@ int     ft_render_screen(t_vars *vars)
     x = 0;
     hit = 0;
     img_ptr = mlx_new_image(vars->mlx_ptr, vars->cub->width, vars->cub->height);
-    vars->addr = mlx_get_data_addr(vars->img_ptr, &vars->bits_per_pixel, &vars->line_length,
+    vars->addr = mlx_get_data_addr(img_ptr, &vars->bits_per_pixel, &vars->line_length,
                                  &vars->endian);
     while (x < vars->cub->width)
     {
