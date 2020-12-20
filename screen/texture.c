71b1fadc78522c7dd22db89d01ac44bbee0c950e
ReiZam <kmazier@student.42.fr>
2020-12-20 11:13:39 +0100

    fix texture

--- screen/texture.c
+++ screen/texture.c
@@ -12,12 +12,9 @@
 
 #include "cub3d.h"
 
-int     ft_load_texture(t_vars *vars, char *path, int index)
+int     ft_load_texture(t_vars *vars, t_img texture[5], char *path, int index)
 {
-    t_img   img;
-
-    img = vars->texture[index];
-    img.img = mlx_xpm_file_to_image(vars->mlx_ptr, path, &img.width, &img.height);
+    texture[index].img = mlx_xpm_file_to_image(vars->mlx_ptr, path, &texture[index].width, &texture[index].height);
     if (img.img == NULL)
         return (0);
     return (1);
@@ -25,9 +22,12 @@ int     ft_load_texture(t_vars *vars, char *path, int index)
 
 void    ft_load_all_texture(t_vars *vars)
 {
-    ft_load_texture(vars, vars->cub->east_texture, 0);
-    ft_load_texture(vars, vars->cub->west_texture, 1);
-    ft_load_texture(vars, vars->cub->north_texture, 2);
-    ft_load_texture(vars, vars->cub->south_texture, 3);
+    t_img   texture[5];
+
+    ft_load_texture(vars, texture, vars->cub->east_texture, 0);
+    ft_load_texture(vars, texture, vars->cub->west_texture, 1);
+    ft_load_texture(vars, texture, vars->cub->north_texture, 2);
+    ft_load_texture(vars, texture, vars->cub->south_texture, 3);
+    vars->texture = texture;
     ft_screen_exit(vars);
 }
