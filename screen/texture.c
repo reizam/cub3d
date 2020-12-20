efd2ef8d30f4f42f92cc44a34effb439ae1cb6c9
ReiZam <kmazier@student.42.fr>
2020-12-20 17:23:39 +0100

    fix texture

--- screen/texture.c
+++ screen/texture.c
@@ -17,7 +17,7 @@ int     ft_load_texture(t_vars *vars, t_img textures[5], char *path, int index)
     textures[index].img_ptr = mlx_xpm_file_to_image(vars->mlx_ptr, path, &textures[index].width, &textures[index].height);
     if (textures[index].img_ptr == NULL)
         return (0);
-    textures[index].addr = (int*)mlx_get_data_addr(img_ptr, &textures[index].bits_per_pixel, &textures[index].line_length, &textures[index].endian);
+    textures[index].addr = (int*)mlx_get_data_addr(textures[index].img_ptr, &textures[index].bits_per_pixel, &textures[index].line_length, &textures[index].endian);
     return (1);
 }
 
