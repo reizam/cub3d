4979cb13ada2b5a5cc95d836a085d11baac4070d
ReiZam <kmazier@student.42.fr>
2020-12-20 11:23:46 +0100

    fix texture

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -71,7 +71,7 @@ typedef struct  s_vars
     int         bits_per_pixel;
     int         line_length;
     int         endian;
-    t_img       *textures;
+    t_img       textures[5];
     t_cub       *cub;
 }               t_vars;
 
--- screen/texture.c
+++ screen/texture.c
@@ -12,18 +12,18 @@
 
 #include "cub3d.h"
 
-int     ft_load_texture(t_vars *vars, t_img *textures, char *path, int index)
+int     ft_load_texture(t_vars *vars, t_img textures[5], char *path, int index)
 {
     textures[index].img = mlx_xpm_file_to_image(vars->mlx_ptr, path, &textures[index].width, &textures[index].height);
+    if (textures[index].img == NULL)
+        return (0);
     return (1);
 }
 
 void    ft_load_all_texture(t_vars *vars)
 {
-    t_img   *textures;
+    t_img   textures[5];
 
-    if (!(textures = (t_img*)malloc(sizeof(t_img) * 5)))
-        return ;
     ft_load_texture(vars, textures, vars->cub->east_texture, 0);
     ft_load_texture(vars, textures, vars->cub->west_texture, 1);
     ft_load_texture(vars, textures, vars->cub->north_texture, 2);
