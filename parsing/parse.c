0e02cf7fa57ac97da3f481a6bb0569526773c88f
ReiZam <kmazier@student.42.fr>
2020-12-21 16:21:40 +0100

    fix

--- parsing/parse.c
+++ parsing/parse.c
@@ -94,8 +94,8 @@ t_cub   *ft_parse_cub_file(char *file)
             return (ft_parse_exit(cub));
         }
     }
-    close(fd);
     if (!ft_is_full(cub) || !ft_parse_map(fd, cub))
         return (ft_parse_exit(cub));
+    close(fd);
     return (cub);
 }
