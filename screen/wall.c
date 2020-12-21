d22e5ed0148e9e1f6bc17ac5eae45bb8a90025fe
ReiZam <kmazier@student.42.fr>
2020-12-21 16:50:58 +0100

    fix vars

--- screen/wall.c
+++ screen/wall.c
@@ -14,7 +14,7 @@
 
 void    ft_draw_wall_line(t_vars *vars, int x, int j[5], double i[8])
 {
-    int     k[4];
+    int     k[5];
     double  wall_x;
     t_img   img;
     
@@ -23,15 +23,16 @@ void    ft_draw_wall_line(t_vars *vars, int x, int j[5], double i[8])
     k[0] = k[0] < 0 ? 0 : k[0];
     k[1] = k[2] / 2 + vars->cub->height / 2;
     k[1] = k[1] >= vars->cub->height ? vars->cub->height - 1 : k[1];
-    if (j[4] == 0)
-        j[4] = j[2] > 0 ? 0 : 1;
-    else
-        j[4] = j[3] > 0 ? 2 : 3;
     wall_x = j[4] == 0 ? (vars->posY + i[7] * i[2]) : (vars->posX + i[7] * i[1]);
     wall_x -= floor((wall_x));
-    img = vars->textures[j[4]];
+    if (j[4] == 0)
+        k[4] = j[2] > 0 ? 0 : 1;
+    else
+        k[4] = j[3] > 0 ? 2 : 3;
+    img = vars->textures[k[4]];
     k[3] = (int)(wall_x * (double)img.width);
-    k[3] = img.width - k[3] - 1;
+    if ((j[4] == 0 && i[1] > 0) || (j[4] == 1 && i[2] < 0))
+        k[3] = img.width - k[3] - 1;
     ft_draw_texture_line(vars, img, x, k);
 }
 
