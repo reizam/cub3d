d31dd78560d9a732602ccd2e4ea574717a4f6b89
ReiZam <kmazier@student.42.fr>
2020-12-22 18:11:18 +0100

    fix wall.c

--- parsing/parse_map.c
+++ parsing/parse_map.c
@@ -62,7 +62,7 @@ int ft_parse_map_param(t_cub *cub)
                 ft_set_spawn(cub, cub->map[i][j]);
             }
     }
-    return (cub->dirX != 0 || cub->dirY != 0);
+    return (cub->dir_x != 0 || cub->dir_y != 0);
 }
 
 int ft_parse_map(int fd, t_cub *cub)
