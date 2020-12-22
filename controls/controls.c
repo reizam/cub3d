58b1238a761e23c8caba731d36354b03e4225c95
ReiZam <kmazier@student.42.fr>
2020-12-22 18:10:41 +0100

    fix wall.c

--- controls/controls.c
+++ controls/controls.c
@@ -15,7 +15,7 @@
 void    ft_move(t_vars *vars, int dir)
 {
     if (vars->cub->map[(int)vars->pos_y][(int)(vars->pos_x + (dir * (vars->dir_x * vars->move_speed)))] != '1')
-        vars->pos_y += dir * (vars->dirX * vars->move_speed);
+        vars->pos_y += dir * (vars->dir_x * vars->move_speed);
     if (vars->cub->map[(int)(vars->pos_y + (dir * (vars->dir_y * vars->move_speed)))][(int)vars->pos_x] != '1')
         vars->pos_y += dir * (vars->dir_y * vars->move_speed);
 }
@@ -27,7 +27,7 @@ void    ft_rot(t_vars *vars, int dir)
     temp = vars->dir_x;
     vars->dir_x = vars->dir_x * cos(vars->rot_speed * dir) - vars->dir_y * sin(vars->rot_speed * dir);
     vars->dir_y = temp * sin(vars->rot_speed * dir) + vars->dir_y * cos(vars->rot_speed * dir);
-    temp = vars->planeX;
+    temp = vars->plane_x;
     vars->plane_x = vars->plane_x * cos(vars->rot_speed * dir) - vars->plane_y * sin(vars->rot_speed * dir);
     vars->plane_x = temp * sin(vars->rot_speed * dir) + vars->plane_y * cos(vars->rot_speed * dir);
 }
