bc26dc68c8e8b4ff1debb4c15ccc5f369b40501d
ReiZam <kmazier@student.42.fr>
2020-12-22 17:02:53 +0100

    map check

--- parsing/parse_map.c
+++ parsing/parse_map.c
@@ -39,7 +39,7 @@ int     ft_check_circle_map(t_cub *cub, int x, int y, int i, int j)
         i += x;
         j += y;
     }
-    return (cub->map[i][j] == '1');
+    return (cub->map[i][j] && cub->map[i][j] == '1');
 }
 
 int     ft_check_map(t_cub *cub)
@@ -53,7 +53,7 @@ int     ft_check_map(t_cub *cub)
         j = -1;
         while (cub->map[i][++j])
         {
-            if (cub->map[i][j] != '1' || cub->map[i][j] != '2')
+            if (cub->map[i][j] != '0')
                 continue ;
             if (!ft_check_circle_map(cub, 0, -1, i, j))
                 return (0);
@@ -65,6 +65,7 @@ int     ft_check_map(t_cub *cub)
                 return (0);
         }
     }
+    return (1);
 }
 
 void    ft_set_spawn(t_cub *cub, char c)
