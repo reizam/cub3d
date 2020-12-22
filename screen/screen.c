6fc392edd68a05688cbd7f93ea1442565b1911ad
ReiZam <kmazier@student.42.fr>
2020-12-22 20:00:33 +0100

    fix

--- screen/screen.c
+++ screen/screen.c
@@ -73,9 +73,9 @@ void    ft_init_vars(t_vars *vars, t_cub *cub)
     vars->cub = cub;
 }
 
-int   ft_leave_hook(t_vars *vars)
+int   ft_leave_hook(void *param)
 {
-    ft_screen_exit(vars);
+    ft_screen_exit((t_vars*)param);
     return (1);
 }
 
@@ -97,8 +97,8 @@ void    ft_open_screen(t_cub *cub)
     vars->img_ptr = mlx_new_image(vars->mlx_ptr, vars->cub->width, vars->cub->height);
     vars->win_ptr = mlx_new_window(vars->mlx_ptr, cub->width, cub->height, "Cub3d");
     vars->addr = mlx_get_data_addr(vars->img_ptr, &vars->bits_per_pixel, &vars->line_length, &vars->endian);
-    mlx_hook(vars->win_ptr, 17, (1L<<17), ft_leave_hook, vars);
-    mlx_hook(vars->win_ptr, 2, (1L<<0), ft_key_hook, vars);
+    mlx_hook(vars->win_ptr, 17, 1L<<17, ft_leave_hook, vars);
+    mlx_hook(vars->win_ptr, 2, 1L<<0, ft_key_hook, vars);
     mlx_do_key_autorepeaton(vars->mlx_ptr);
     mlx_loop_hook(vars->mlx_ptr, ft_render_screen, vars);
     mlx_loop(vars->mlx_ptr);
