583eccc85c770f0cb82cf269cd23d241c7a01f07
ReiZam <kmazier@student.42.fr>
2020-12-23 15:19:41 +0100

    fix bitmap.c

--- image/bitmap.c
+++ image/bitmap.c
@@ -14,7 +14,7 @@
 
 unsigned char *ft_create_bmp_file_header(t_vars *vars, int pitch)
 {
-    unsigned char   file_header[14] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    static unsigned char   file_header[14] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
     int             file_size;
 
     file_size = 54 + ((pitch) + (4 - (pitch) % 4) % 4) * vars->cub->height;
@@ -31,7 +31,7 @@ unsigned char *ft_create_bmp_file_header(t_vars *vars, int pitch)
 
 unsigned char *ft_create_bmp_info_header(t_vars *vars)
 {
-    unsigned char info_header[40] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    static unsigned char info_header[40] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 
     info_header[0] = (unsigned char)(40);
     info_header[4] = (unsigned char)(vars->cub->width);
@@ -43,7 +43,7 @@ unsigned char *ft_create_bmp_info_header(t_vars *vars)
     info_header[10] = (unsigned char)(vars->cub->height >> 16);
     info_header[11] = (unsigned char)(vars->cub->height >> 24);
     info_header[12] = (unsigned char)(1);
-    info_header[14] = (unsigned char)(vars->bits_per_pixel);
+    info_header[14] = (unsigned char)(3 * 8);
     return (info_header);
 }
 
@@ -56,11 +56,10 @@ void    ft_save_image(t_vars *vars, char *file_name)
     int             pitch;
     int             i;
     int             j;
-    static unsigned char rgb[3] = {0, 0, 0};
 
     i = -1;
     cpy = (unsigned char*)vars->addr;
-    pitch = (vars->bits_per_pixel / 8) * vars->cub->width;
+    pitch = 3 * vars->cub->width;
     info_header = ft_create_bmp_file_header(vars, pitch);
     file_header = ft_create_bmp_info_header(vars);
     fd = open(file_name, O_WRONLY | O_CREAT, 0777);
@@ -71,12 +70,9 @@ void    ft_save_image(t_vars *vars, char *file_name)
         j = -1;
         while (++j < vars->cub->width)
         {
-            rgb[0] = (cpy[(vars->cub->height - i) * (vars->cub->width) + j]) >> 16;
-            rgb[1] = (cpy[(vars->cub->height - i) * (vars->cub->width) + j]) >> 8;
-            rgb[2] = (cpy[(vars->cub->height - i) * (vars->cub->width) + j]);
-            write(fd, rgb + 2, 1);
-            write(fd, rgb + 1, 1);
-            write(fd, rgb, 1);
+            write(fd, &(cpy[(vars->cub->height - i) * (vars->cub->width) + j]) >> 16, 1);
+            write(fd, &(cpy[(vars->cub->height - i) * (vars->cub->width) + j]) >> 8, 1);
+            write(fd, &(cpy[(vars->cub->height - i) * (vars->cub->width) + j]), 1);
         }
     }
     close(fd);
