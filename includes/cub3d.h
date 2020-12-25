4e3466458a132a29289b1c941ad4b1405064611c
ReiZam <kmazier@student.42.fr>
2020-12-26 00:02:44 +0100

    norme & fix

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -1,13 +1,13 @@
 /* ************************************************************************** */
-/*																			*/
-/*														:::	  ::::::::   */
-/*   cub3d.h											:+:	  :+:	:+:   */
-/*													+:+ +:+		 +:+	 */
-/*   By: kmazier <kmazier@student.42.fr>			+#+  +:+	   +#+		*/
-/*												+#+#+#+#+#+   +#+		   */
-/*   Created: 2020/12/15 13:03:25 by kmazier		   #+#	#+#			 */
-/*   Updated: 2020/12/15 18:01:13 by kmazier		  ###   ########.fr	   */
-/*																			*/
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3d.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kmazier <kmazier@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/26 00:00:33 by kmazier           #+#    #+#             */
+/*   Updated: 2020/12/26 00:00:33 by kmazier          ###   ########.fr       */
+/*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CUB3D_H
@@ -27,14 +27,14 @@
 # include "mlx.h"
 # include <math.h>
 
-typedef struct  s_sprite
+typedef struct  	s_sprite
 {
 	int			 	x;
 	int			 	y;
-	struct s_sprite *next;
-}				   t_sprite;
+	struct s_sprite	*next;
+}					t_sprite;
 
-typedef struct  s_img
+typedef struct	s_img
 {
 	char		*img_ptr;
 	int		 	*addr;
@@ -43,7 +43,7 @@ typedef struct  s_img
 	int		 	bits_per_pixel;
 	int		 	line_length;
 	int		 	endian;
-}			   t_img;
+}				t_img;
 
 typedef struct	s_cub
 {
@@ -70,26 +70,26 @@ typedef struct	s_cub
 
 typedef struct  s_vars
 {
-	void	*mlx_ptr;
-	void	*win_ptr;
-	void	*img_ptr;
-	double  pos_x;
-	double  pos_y;
-	double  dir_x;
-	double  dir_y;
-	double  plane_x;
-	double  plane_y;
-	double  move_speed;
-	double  rot_speed;
-	char	*addr;
-	int	 	bits_per_pixel;
-	int	 	line_length;
-	int	 	endian;
-	int	 	keys[4];
-	t_img   textures[5];
-	double  *h_buffer;
-	t_cub   *cub;
-}			   t_vars;
+	void		*mlx_ptr;
+	void		*win_ptr;
+	void		*img_ptr;
+	double		pos_x;
+	double		pos_y;
+	double		dir_x;
+	double		dir_y;
+	double		plane_x;
+	double		plane_y;
+	double		move_speed;
+	double		rot_speed;
+	char		*addr;
+	int			bits_per_pixel;
+	int			line_length;
+	int			endian;
+	int			keys[4];
+	t_img		textures[5];
+	double		*h_buffer;
+	t_cub   	*cub;
+}				t_vars;
 
 void		ft_draw_pixel_if_not_black(t_vars *vars, int x, int y, int color);
 void		ft_sort_sprites(t_sprite **begin, int pos_x, int pos_y);
@@ -98,36 +98,36 @@ void		ft_lstadd_front(t_sprite **alst, int x, int y);
 t_sprite	*ft_create_elem(void);
 void		ft_save_image(t_vars *vars, char *file_name);
 void		ft_do_controls(t_vars *vars);
-int		 	ft_key_release(int keycode, t_vars *vars);
-int		 	ft_key_press(int keycode, t_vars *vars);
-int		 	ft_check_map_line(char *line);
-int		 	ft_check_map(t_cub *cub);
-int		 	ft_is_map_char(char c, int just_spawn);
+int			ft_key_release(int keycode, t_vars *vars);
+int			ft_key_press(int keycode, t_vars *vars);
+int			ft_check_map_line(char *line);
+int			ft_check_map(t_cub *cub);
+int			ft_is_map_char(char c, int just_spawn);
 size_t		ft_strlen(const char *s);
 void		ft_putstr_fd(char *s, int fd);
 int		 	ft_strcmp(char *s1, char *s2);
-int		 	ft_strncmp(char *s1, char *s2, size_t n);
-int		 	ft_check_file_ext(char *file, char *extension);
+int			ft_strncmp(char *s1, char *s2, size_t n);
+int			ft_check_file_ext(char *file, char *extension);
 int			get_next_line(int fd, char **line);
-int		 	ft_parse_map(int fd, t_cub *cub);
+int			ft_parse_map(int fd, t_cub *cub);
 int			ft_nb_len(int nb);
-t_cub	   *ft_parse_exit(t_cub *cub);
+t_cub		*ft_parse_exit(t_cub *cub);
 int			ft_is_full(t_cub *cub);
 int			ft_atoi(char *str);
-int		 	ft_parse_rgb(char *line, int id, t_cub *cub);
-t_cub	   *ft_parse_cub_file(char *file);
-int		 	ft_isnum(char *str);
+int			ft_parse_rgb(char *line, int id, t_cub *cub);
+t_cub		*ft_parse_cub_file(char *file);
+int			ft_isnum(char *str);
 char		*ft_strjoin(char *s1, char *s2, int n);
-int		 	ft_check_rgb(int r, int g, int b);
+int			ft_check_rgb(int r, int g, int b);
 char		**ft_split(char const *s, char c);
 char		*ft_strdup(char *src);
 void		ft_open_screen(t_cub *cub);
 void		ft_draw_pixel(t_vars *vars, int x, int y, int color);
 void		ft_draw_ver_line(t_vars *vars, int x, int start_y, int end_y, int color);
 void		ft_screen_exit(t_vars *vars);
-int		 	ft_parse_texture(char *line, int id, t_cub *cub);
+int			ft_parse_texture(char *line, int id, t_cub *cub);
 void		ft_print_error(char *error);
-int		 	ft_load_all_texture(t_vars *vars);
+int			ft_load_all_texture(t_vars *vars);
 void		ft_draw_texture_line(t_vars *vars, t_img img, int x, int k[4]);
 void		ft_screen_init_exit(t_vars *vars, int do_exit);
 void		ft_draw_wall(t_vars *vars, double **h_buffer);
