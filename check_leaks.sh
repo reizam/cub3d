fdbc2c897aaddc6049384039f5a3ebdbbb5e6ceb
ReiZam <kmazier@student.42.fr>
2020-12-25 22:57:09 +0100

    clean-up project

--- check_leaks.sh
+++
@@ -1,1 +0,0 @@
-EXEC=./cub3D;ARGS=test.cub;valgrind --tool=memcheck --leak-check=full --leak-resolution=high --show-reachable=yes --log-file=valgrind_log ./$EXEC $ARGS;grep -A1 "valgrind" valgrind_log|grep $EXEC
