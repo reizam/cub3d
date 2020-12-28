58ea49e55ee583568122cbd84770b8d77c1d3c62
ReiZam <kmazier@student.42.fr>
2020-12-28 17:34:53 +0100

    add fix

--- parsing/parse_check_map.c
+++ parsing/parse_check_map.c
@@ -30,13 +30,13 @@ int	ft_check_circle_map(t_cub *cub, int type, int i, int j)
 
 	x = 0;
 	y = 0;
-	if (type == 0)
+	if (type == 0 || type == 4 || type == 5)
 		x = -1;
-	else if (type == 1)
+	if (type == 1 || type == 5 || type == 7)
 		x = 1;
-	else if (type == 2)
+	if (type == 2 || type == 6 || type == 7)
 		y = 1;
-	else if (type == 3)
+	if (type == 3 || type == 4 || type == 6)
 		y = -1;
 	while (j >= 0 && i >= 0 && cub->map[i]
 	&& cub->map[i][j] && cub->map[i][j] != '1')
@@ -64,13 +64,10 @@ int	ft_check_map(t_cub *cub)
 			if (cub->map[i][j] != '0' && cub->map[i][j] != '2'
 			&& !ft_is_map_char(cub->map[i][j], 1))
 				continue ;
-			if (!ft_check_circle_map(cub, 0, i, j))
-				return (0);
-			if (!ft_check_circle_map(cub, 1, i, j))
-				return (0);
-			if (!ft_check_circle_map(cub, 2, i, j))
-				return (0);
-			if (!ft_check_circle_map(cub, 3, i, j))
+			if (!ft_check_circle_map(cub, 0, i, j) || !ft_check_circle_map(cub, 1, i, j)
+			|| !ft_check_circle_map(cub, 2, i, j) || !ft_check_circle_map(cub, 3, i, j)
+			|| !ft_check_circle_map(cub, 4, i, j) || !ft_check_circle_map(cub, 5, i, j)
+			|| !ft_check_circle_map(cub, 6, i, j) || !ft_check_circle_map(cub, 7, i, j))
 				return (0);
 		}
 	}
